960598a1b67bdd77d07ae8f9dafbbd47ad6bb667
iloominate <aliakseiklimau@gmail.com>
2022-04-19 00:25:24 +0200

    Comments added

--- gui.py
+++ gui.py
@@ -2,8 +2,10 @@ from glob import glob
 import tkinter as tk
 from tkinter import font 
 
+# string for input symbols 
 calculation = ""
 
+# function adds symbol to calculation string 
 def add_to_calculation(symbol):
     global calculation
     calculation += str(symbol)
@@ -11,7 +13,7 @@ def add_to_calculation(symbol):
     text_result.insert(1.0, calculation)
     
 
-
+# 
 def evaluate_calculation():
     global calculation
     print(calculation)
@@ -28,7 +30,7 @@ def evaluate_calculation():
 
     pass
 
-
+# clears string with calculation
 def clear_field():
     global calculation
     calculation = ""
@@ -36,9 +38,11 @@ def clear_field():
     text_result.insert(1.0, "")
     pass
 
+# window for interface
 root = tk.Tk()
 root.geometry("500x400")
 
+# row and column configure make buttons sreatchable
 tk.Grid.rowconfigure(root, 0, weight=1)
 tk.Grid.columnconfigure(root, 0, weight=1)
 text_result = tk.Text(root, height=2, width=22, font=("Arial", 24))
@@ -57,16 +61,16 @@ tk.Grid.columnconfigure(root, 5, weight=1)
 tk.Grid.rowconfigure(root, 6, weight=1)
 tk.Grid.columnconfigure(root, 6, weight=1)
 
+
+# buttons for numbers and symbols
 btn_1 = tk.Button(root, text="1", command = lambda: add_to_calculation(1), width=5, font=("Arial", 14))
 btn_1.grid(row=3, column=1, sticky = "nswe")
 tk.Grid.rowconfigure(root, 2, weight=1)
 tk.Grid.columnconfigure(root, 2, weight=1)
-
 btn_2 = tk.Button(root, text="2", command = lambda: add_to_calculation(2), width=5, font=("Arial", 14))
 btn_2.grid(row=3, column=2, sticky = "nswe")
 tk.Grid.rowconfigure(root, 3, weight=1)
 tk.Grid.columnconfigure(root, 3, weight=1)
-
 btn_3 = tk.Button(root, text="3", command = lambda: add_to_calculation(3), width=5, font=("Arial", 14))
 btn_3.grid(row=3, column=3, sticky = "nswe")
 btn_4 = tk.Button(root, text="4", command = lambda: add_to_calculation(4), width=5, font=("Arial", 14))
@@ -83,17 +87,16 @@ btn_9 = tk.Button(root, text="9", command = lambda: add_to_calculation(9), width
 btn_9.grid(row=5, column=3, sticky = "nswe")
 btn_0 = tk.Button(root, text="0", command = lambda: add_to_calculation(0), width=5, font=("Arial", 14))
 btn_0.grid(row=6, column=1, sticky = "nswe")
-
 btn_dot = tk.Button(root, text=".", command = lambda: add_to_calculation("."), width=5, font=("Arial", 14))
 btn_dot.grid(row=6, column=2, sticky = "nswe")
-btn_equal = tk.Button(root, text="=", command = evaluate_calculation, width=5, font=("Arial", 14), bg="#0098ED")
-btn_equal.grid(row=6, column=3, sticky = "nswe")
-
 btn_left_parenthesis = tk.Button(root, text="(", command = lambda: add_to_calculation("("), width=5, font=("Arial", 14))
 btn_left_parenthesis.grid(row=2, column=1, sticky = "nswe")
-
 btn_right_parenthesis = tk.Button(root, text=")", command = lambda: add_to_calculation(")"), width=5, font=("Arial", 14))
 btn_right_parenthesis.grid(row=2, column=2, sticky = "nswe")
+
+# buttons for operations 
+btn_equal = tk.Button(root, text="=", command = evaluate_calculation, width=5, font=("Arial", 14), bg="#0098ED")
+btn_equal.grid(row=6, column=3, sticky = "nswe")
 
 btn_clear = tk.Button(root, text="C", command = clear_field, width=6, font=("Arial", 14), bg='#CB0535')
 btn_clear.grid(row=2, column=5, sticky = "nswe")
@@ -111,9 +114,6 @@ btn_root.grid(row=2, column=3, sticky = "nswe")
 btn_power = tk.Button(root, text="x\u02b8", command = lambda: add_to_calculation("^"), width=5, font=("Arial", 14))
 btn_power.grid(row=2, column=4, sticky = "nswe")
 
-# btn_advanced_mod = tk.Button(root, text="Advanced", command = lambda: add_to_calculation(1), width=5, font=("Arial", 14))
-# btn_advanced_mod.grid(rowspan = 2, row = 3, column=5)
-
 btn_devide = tk.Button(root, text="÷", command = lambda: add_to_calculation("/"), width=5, font=("Arial", 14))
 btn_devide.grid(row=3, column=4, sticky = "nswe")
 
@@ -127,6 +127,7 @@ btn_minus = tk.Button(root, text="-", command = lambda: add_to_calculation("-"),
 btn_minus.grid(row=6, column=4, sticky = "nswe") 
 
 
+# defines min size for calculator window 
 root.wm_minsize(400, 250)
 
 root.mainloop()
