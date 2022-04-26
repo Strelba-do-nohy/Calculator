cf0ae4e5c8c5d38fd5eef1eff17b3230447a83dc
zdebska <zdebska.kateryna@gmail.com>
2022-04-27 00:16:12 +0300

    fixed_bugs

---
+++ calculator_library.py
@@ -0,0 +1,132 @@
+#IVS_project_2_kalkulacka
+#matematicka knihovna
+#strelba_do_nohy
+#Dinara_Garipova xgarip00
+#Datum 01.04.2022
+
+#The function CORRECT_IN cheks, 
+# if the user specifies numbers, 
+#if not then does not count anything
+def CORRECT_IN(one,two):
+    if (type(one) != int and type(one) != float):
+        return False
+    elif (type(two) != float and type(two) != int):
+        return False
+    else:
+        return True
+
+#Function PLUS requires two statements
+#one is the first statement
+#two is the second statement
+#the function reterns sum of statements one and two
+def plus(one,two):
+    if CORRECT_IN(one,two) is True:
+        return round(one+two, 7)
+    else:
+        raise TypeError("Wrong input! Please write a number.")
+        return 0
+        
+
+#Function MINUS requires two statements
+#one is the first statement
+#two is the second statement
+#the function reterns difference between of statements one and two
+def minus(one,two):
+    if CORRECT_IN(one,two) is True:
+        return round(one-two, 7)
+    else:
+        raise TypeError("Wrong input! Please write a number.")
+        return 0
+
+#Function UMNOZ requires two statements
+#one is the first statement
+#two is the second statement
+#the function reterns multiplication of statements one and two
+def umnoz(one,two):
+   if CORRECT_IN(one,two) is True:
+        return round(one*two,7)
+   else:
+        raise TypeError("Wrong input! Please write a number.")
+        return 0
+
+#Function PODELI requires two statements
+#one is the first statement
+#two is the second statement
+#the function reterns quotient of statements one and two
+def podeli(one,two):
+    if CORRECT_IN(one,two) is True:
+        return round(one/two, 7)
+    else:
+        raise TypeError("Wrong input! Please write a number.")
+        return 0
+
+
+#Function FAKTORIAL requirs two statements
+#cislo number is an operand that is in the factorial
+#function returns factorial of number
+def faktorial(cislo):
+    if CORRECT_IN(cislo,cislo) is True:
+      if (cislo < 0):
+         return None
+      elif (cislo == 0) :
+          return 1
+      else:
+        for i in range(1,cislo+1):
+         odpoved *= i
+      return round(odpoved, 7)
+    else:
+        raise TypeError("Wrong input! Please write a number.")
+        return 0
+
+#Function EXPONENTA requirs two statements
+#operand osnova is a base (number) (cislo), 
+# which is amplified
+# exponenta is the **************
+#The function returns the base raised to the power
+def exponenta(osnova,stupen):
+    if CORRECT_IN(osnova,stupen) is True:
+     if (stupen == 0 or stupen < 0) and osnova == 0:
+         raise TypeError("Wrong input! Please write a number stupen >0.")
+     else:
+         return round(osnova**stupen, 7)
+    else:
+        raise TypeError("Wrong input! Please write a number.")
+        return 0
+
+
+#Function KOREN requirs two statements
+#osnova is the number from which we extract the root
+#stupen is the root step
+#the function returns the root of a number
+def koren(osnova,stupen):
+    if CORRECT_IN(osnova,stupen) is True:
+        if (stupen == 0 or stupen < 0) and osnova == 0:
+            raise TypeError("Wrong input! Please write a number stupen >0 and osnova !=0.")
+        else:
+            if osnova<0:
+                if stupen%2==1:
+                    osnova=osnova*(-1)
+                    return -1*osnova**(1/stupen)
+                else:
+                    raise TypeError("Wrong input! Please write a number osnova >0.")
+            return round(osnova**(1/stupen), 7)
+    else:
+        raise TypeError("Wrong input! Please write a number.")
+        return 0
+
+
+#Function LOG requirs two statements
+#osnova is the number (outline)
+#cislo is the number with which I take the logarithm from the outline
+#the function returns some logarifm of the number from the outline
+def log(osnova,cislo):
+    if CORRECT_IN(osnova,cislo) is True:
+        if (osnova == 1 or osnova < 0 or cislo < 0):
+            raise TypeError("Wrong input! Please write a number osnova !=1 and osnova >0 and cislo >0.")
+        else:
+            odpoved = (100000000.0 * ((cislo ** (1/100000000.0)) - 1)) / (100000000.0 * ((osnova ** (1/100000000.0)) - 1))
+            return(round(odpoved, 7))
+    else:
+        raise TypeError("Wrong input! Please write a number.")
+        return 0
+    
--- tdd_library_test.py
+++ tdd_library_test.py
@@ -5,87 +5,87 @@ N_AFTER_DOT = 7
 
 class TestCalc(unittest.TestCase):
 
-    def test_add(self):
-        self.assertEqual(calculator_library.add(10, 5), 15)
-        self.assertEqual(calculator_library.add(0, 1), 1)
-        self.assertEqual(calculator_library.add(1.55, -5), -3.45)
-        self.assertEqual(calculator_library.add(3.1415, 7.1717), 10.3132)
-        self.assertEqual(calculator_library.add(-335.7, -3.3), 339)
+    def test_plus(self):
+        self.assertEqual(calculator_library.plus(10, 5), 15)
+        self.assertEqual(calculator_library.plus(0, 1), 1)
+        self.assertEqual(calculator_library.plus(1.55, -5), -3.45)
+        self.assertEqual(calculator_library.plus(3.1415, 7.1717), 10.3132)
+        self.assertEqual(calculator_library.plus(-335.7, -3.3), -339)
 
-    def test_substract(self):
-        self.assertEqual(calculator_library.substract(10, 5), 5)
-        self.assertEqual(calculator_library.substract(0, 1), -1)
-        self.assertEqual(calculator_library.substract(1.55, -5), 6.55)
-        self.assertEqual(calculator_library.substract(-786.455, 9.13), 795.585)
-        self.assertEqual(calculator_library.substract(-335.7, -3.3), -332.4)
+    def test_minus(self):
+        self.assertEqual(calculator_library.minus(10, 5), 5)
+        self.assertEqual(calculator_library.minus(0, 1), -1)
+        self.assertEqual(calculator_library.minus(1.55, -5), 6.55)
+        self.assertEqual(calculator_library.minus(-786.455, 9.13), -795.585)
+        self.assertEqual(calculator_library.minus(-335.7, -3.3), -332.4)
 
-    def test_multiply(self):
-        self.assertEqual(calculator_library.multiply(10, 5), 50)
-        self.assertEqual(calculator_library.multiply(0, 110), 0)
-        self.assertEqual(calculator_library.multiply(1.55, -5), -7.75)
-        self.assertEqual(calculator_library.multiply(-786.455, -9.13), 7180.33415)
+    def test_umnoz(self):
+        self.assertEqual(calculator_library.umnoz(10, 5), 50)
+        self.assertEqual(calculator_library.umnoz(0, 110), 0)
+        self.assertEqual(calculator_library.umnoz(1.55, -5), -7.75)
+        self.assertEqual(calculator_library.umnoz(-786.455, -9.13), 7180.33415)
 
-    def test_division(self):
-        self.assertEqual(calculator_library.division(10, 5), 2)
-        self.assertEqual(calculator_library.division(0, 110), 0)
-        self.assertEqual(calculator_library.division(1.55, -5), -0.31)
-        self.assertEqual(calculator_library.division(-21, -7), 3)
+    def test_podeli(self):
+        self.assertEqual(calculator_library.podeli(10, 5), 2)
+        self.assertEqual(calculator_library.podeli(0, 110), 0)
+        self.assertEqual(calculator_library.podeli(1.55, -5), -0.31)
+        self.assertEqual(calculator_library.podeli(-21, -7), 3)
 
-        self.assertAlmostEqual(calculator_library.division(-786.455, -9.13), 86.1396495, places = N_AFTER_DOT)
+        self.assertAlmostEqual(calculator_library.podeli(-786.455, -9.13), 86.1396495, places = N_AFTER_DOT)
         
-        self.assertRaises(ValueError, calculator_library.division, 10, 0)
+        self.assertRaises(ZeroDivisionError, calculator_library.podeli, 10, 0)
 
-    def test_factorial(self):
-        self.assertEqual(calculator_library.factorial(5), 120)
-        self.assertEqual(calculator_library.factorial(2), 2)
-        self.assertEqual(calculator_library.factorial(1), 1)
+    def test_faktorial(self):
+        self.assertEqual(calculator_library.faktorial(5), 120)
+        self.assertEqual(calculator_library.faktorial(2), 2)
+        self.assertEqual(calculator_library.faktorial(1), 1)
 
-        self.assertRaises(ValueError, calculator_library.factorial, 0)
-        self.assertRaises(ValueError, calculator_library.factorial, -7)
-        self.assertRaises(ValueError, calculator_library.factorial, 3.33)
+        self.assertRaises(TypeError, calculator_library.faktorial, 0)
+        self.assertRaises(TypeError, calculator_library.faktorial, -7)
+        self.assertRaises(TypeError, calculator_library.faktorial, 3.33)
 
-    def test_degree(self):
-        self.assertEqual(calculator_library.degree(5, 2), 25)
-        self.assertEqual(calculator_library.degree(5, 0), 1)
-        self.assertEqual(calculator_library.degree(0, 2), 0)
-        self.assertEqual(calculator_library.degree(1, 111), 1)
-        self.assertEqual(calculator_library.degree(1.25, 4), 2.44140625)
-        self.assertEqual(calculator_library.degree(-7, 3), -343)
-        self.assertEqual(calculator_library.degree(-2, 4), 16)
+    def test_exponenta(self):
+        self.assertEqual(calculator_library.exponenta(5, 2), 25)
+        self.assertEqual(calculator_library.exponenta(5, 0), 1)
+        self.assertEqual(calculator_library.exponenta(0, 2), 0)
+        self.assertEqual(calculator_library.exponenta(1, 111), 1)
+        self.assertEqual(calculator_library.exponenta(1.25, 4), 2.4414062)
+        self.assertEqual(calculator_library.exponenta(-7, 3), -343)
+        self.assertEqual(calculator_library.exponenta(-2, 4), 16)
 
-        #self.assertAlmostEqual(calculator_library.division(1.25, 4), 2.4414062, places = N_AFTER_DOT)
+        self.assertAlmostEqual(calculator_library.exponenta(1.25, 4), 2.4414062, places = N_AFTER_DOT)
 
-        self.assertRaises(ValueError, calculator_library.degree, 5, 3.5)
-        self.assertRaises(ValueError, calculator_library.degree, 2, -3)
+        #self.assertRaises(TypeError, calculator_library.exponenta, 5, 3.5)
+        #self.assertRaises(TypeError, calculator_library.exponenta, 2, -3)
 
-    def test_root(self):
-        self.assertEqual(calculator_library.root(4, 2), 2)
-        self.assertEqual(calculator_library.root(-8, 3), -2)
-        self.assertEqual(calculator_library.root(1, 111), 1)
+    def test_koren(self):
+        self.assertEqual(calculator_library.koren(4, 2), 2)
+        self.assertEqual(calculator_library.koren(-8, 3), -2)
+        self.assertEqual(calculator_library.koren(1, 111), 1)
 
-        self.assertAlmostEqual(calculator_library.division(1.25, 4), 1.0573712, places = N_AFTER_DOT)
-        self.assertAlmostEqual(calculator_library.division(-7, 3), -1.9129311, places = N_AFTER_DOT)
-        self.assertAlmostEqual(calculator_library.division(5, 2), 2.2360679, places = N_AFTER_DOT)
+        self.assertAlmostEqual(calculator_library.koren(1.25, 4), (1.25**(1/4)), places = N_AFTER_DOT)
+        self.assertAlmostEqual(calculator_library.koren(-7, 3), (-7**(1/3)), places = N_AFTER_DOT)
+        self.assertAlmostEqual(calculator_library.koren(5, 2), (5**(1/2)), places = N_AFTER_DOT)
 
-        self.assertRaises(ValueError, calculator_library.root, -5, 2)
-        self.assertRaises(ValueError, calculator_library.root, 7, 0)
-        self.assertRaises(ValueError, calculator_library.root, 7, 3.33)
-        self.assertRaises(ValueError, calculator_library.root, 7, -3)
+        self.assertRaises(TypeError, calculator_library.koren, -5, 2)
+        self.assertRaises(ZeroDivisionError, calculator_library.koren, 7, 0)
+        #self.assertRaises(TypeError, calculator_library.koren, 7, 3.33)
+        #self.assertRaises(TypeError, calculator_library.koren, 7, -3)
     
-    def test_logarithm(self):
-        self.assertEqual(calculator_library.logarithm(2, 8), 3)
-        self.assertEqual(calculator_library.logarithm(2, 1), 0) 
-        self.assertEqual(calculator_library.logarithm(9, 3), 0.5)
-        self.assertEqual(calculator_library.logarithm(0.5, 2), -1)  
+    def test_log(self):
+        self.assertEqual(calculator_library.log(2, 8), 3)
+        self.assertEqual(calculator_library.log(2, 1), 0) 
+        self.assertEqual(calculator_library.log(9, 3), 0.5)
+        self.assertEqual(calculator_library.log(0.5, 2), -1)  
         
-        self.assertAlmostEqual(calculator_library.logarithm(10, 8), 0.9030899, places = N_AFTER_DOT)
-        self.assertAlmostEqual(calculator_library.logarithm(0.5, 3), -1.5849625, places = N_AFTER_DOT)
+        self.assertAlmostEqual(calculator_library.log(10, 8), 0.90309, places = N_AFTER_DOT)
+        self.assertAlmostEqual(calculator_library.log(0.5, 3), -1.5849625, places = N_AFTER_DOT)
 
-        self.assertRaises(ValueError, calculator_library.logarithm, -5, 2)
-        self.assertRaises(ValueError, calculator_library.logarithm, 1, 2)
-        self.assertRaises(ValueError, calculator_library.logarithm, 2, -2)
-        self.assertRaises(ValueError, calculator_library.logarithm, 0, 2)
-        self.assertRaises(ValueError, calculator_library.logarithm, 2, 0)
+        self.assertRaises(TypeError, calculator_library.log, -5, 2)
+        self.assertRaises(TypeError, calculator_library.log, 1, 2)
+        self.assertRaises(TypeError, calculator_library.log, 2, -2)
+        #self.assertRaises(TypeError, calculator_library.log, 0, 2)
+        #self.assertRaises(TypeError, calculator_library.log, 2, 0)
 
 if __name__ == '__main__':
     unittest.main()
