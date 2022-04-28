a45675101e0b3565ed386de8ac0e4e9d0841d48a
AnotherAlina <101659368+AnotherAlina@users.noreply.github.com>
2022-04-28 21:05:54 +0200

    Add files via upload

--- input_output.py
+++ input_output.py
@@ -1,106 +1,5 @@
+import matematicka_knihovna
 import re
-
-def CORRECT_IN(one,two):
-    if (type(one) != int and type(one) != float):
-        return False
-    elif (type(two) != float and type(two) != int):
-        return False
-    else:
-        return True
-        
-def minus(one,two):
-    if CORRECT_IN(one,two) is True:
-        return one-two
-    else:
-        raise TypeError("Wrong input! Please write a number.")
-        return 0
-        
-def plus(one,two):
-    if CORRECT_IN(one,two) is True:
-        return one+two
-    else:
-        raise TypeError("Wrong input! Please write a number.")
-        return 0
-
-def umnoz(one,two):
-   if CORRECT_IN(one,two) is True:
-        return one*two
-   else:
-        raise TypeError("Wrong input! Please write a number.")
-        return 0
-        
-def podeli(one,two):
-    if CORRECT_IN(one,two) is True:
-        return one/two
-    else:
-        raise TypeError("Wrong input! Please write a number.")
-        return 0
-
-def faktorial(cislo):
-    if CORRECT_IN(cislo,cislo) is True:
-      if (cislo < 0):
-         return None
-      elif (cislo == 0) :
-          return 1
-      else:
-        odpoved = 1
-        for i in range(1,cislo+1):
-         odpoved = odpoved * i
-      return round(odpoved, 7)
-    else:
-        raise TypeError("Wrong input! Please write a number.")
-        return 0
-
-
-# def log(osnova,cislo):
-#     if CORRECT_IN(osnova,cislo) is True:
-#         if (osnova == 1 or osnova < 0 or cislo < 0):
-#             raise TypeError("Wrong input! Please write a number osnova !=1 and osnova >0 and cislo >0.")
-#         else:
-#             for i in range(1,cislo):
-#                 if (osnova**i==cislo):
-#                     return i
-#     else:
-#         raise TypeError("Wrong input! Please write a number.")
-#         return 0
-
-def log(osnova,cislo):
-    if CORRECT_IN(osnova,cislo) is True:
-        if (osnova == 1 or osnova < 0 or cislo < 0):
-            raise TypeError("Wrong input! Please write a number osnova !=1 and osnova >0 and cislo >0.")
-        else:
-            odpoved = (100000000.0 * ((cislo ** (1/100000000.0)) - 1)) / (100000000.0 * ((osnova ** (1/100000000.0)) - 1))
-            return(round(odpoved, 7))
-    else:
-        raise TypeError("Wrong input! Please write a number.")
-        return 0
-
-def koren(osnova,stupen):
-    if CORRECT_IN(osnova,stupen) is True:
-        if (stupen == 0 or stupen < 0) and osnova == 0:
-            raise TypeError("Wrong input! Please write a number stupen >0 and osnova !=0.")
-        else:
-            if osnova<0:
-                if stupen%2==1:
-                    osnova=osnova*(-1)
-                    return -1*osnova**(1/stupen)
-                else:
-                    raise TypeError("Wrong input! Please write a number osnova >0.")
-            return osnova**(1/stupen)
-    else:
-        raise TypeError("Wrong input! Please write a number.")
-        return 0
-        
-def exponenta(osnova,stupen):
-    if CORRECT_IN(osnova,stupen) is True:
-     if (stupen == 0 or stupen < 0) and osnova == 0:
-         raise TypeError("Wrong input! Please write a number stupen >0.")
-     else:
-         return osnova**stupen
-    else:
-        raise TypeError("Wrong input! Please write a number.")
-        return 0
-########
 
 def parse(line):
     change = re.findall(r'log[0-9]+|log /[0-9]+', line) + re.findall(r'log/[0-9]+', line)
@@ -166,7 +65,7 @@ def check_exp(result):
                     y = float("-" + exp[1])
             else:
                 y = float(exp[1])
-            result[item] = str(exponenta(x,y))
+            result[item] = str(matematicka_knihovna.exponenta(x,y))
             if re.match(r'-', result[item]):
                 tmp_exp = result[item]
                 result[item] = "/" + tmp_exp[1:]
@@ -179,19 +78,19 @@ def check_fact(result):
             fact = re.split(r'\!', result[item])
             if fact[0].isdigit():
                  x = int(fact[0])
-                 result[item] = str(faktorial(x))
+                 result[item] = str(matematicka_knihovna.faktorial(x))
             elif re.match(r'/', fact[0]):
                 tmp_fact = fact[0]
                 fact[0] = tmp_fact[1:]
                 if fact[0].isdigit():
                     x = int(fact[0])
-                    result[item] = "/" + str(faktorial(x))
+                    result[item] = "/" + str(matematicka_knihovna.faktorial(x))
                 else:
                     x = float(fact[0])
-                    result[item] = "/" + str(faktorial(x))
+                    result[item] = "/" + str(matematicka_knihovna.faktorial(x))
             else:
                 x = float(fact[0])
-                result[item] = str(faktorial(x))
+                result[item] = str(matematicka_knihovna.faktorial(x))
     return result
     
 def check_kor(result):
@@ -224,9 +123,9 @@ def check_kor(result):
             else:
                 y = float(kor[1])
             if flag_min:
-                result[item] = "/" + str(koren(y, x))
+                result[item] = "/" + str(matematicka_knihovna.koren(y, x))
             else:
-                result[item] = str(koren(y, x))
+                result[item] = str(matematicka_knihovna.koren(y, x))
             if re.match(r'-', result[item]):
                 result[item] = "/" + result[item][1:]
             if re.match(r'/-', result[item]):
@@ -263,7 +162,7 @@ def check_log(result):
                     y = int(loga[1])
                 else:
                     y = float(loga[1])
-            result[item] = str(log(x,y))
+            result[item] = str(matematicka_knihovna.log(x,y))
             if re.match(r'-', result[item]):
                 result[item] = "/" + result[item][1:]
     return result
@@ -304,7 +203,7 @@ def check_mul(result):
                         y = float("-" + result[item+1])
                     else:
                         y = float(result[item+1])
-                result[item+1] = str(umnoz(x,y))
+                result[item+1] = str(matematicka_knihovna.umnoz(x,y))
                 if re.match(r'-', result[item+1]):
                     result[item+1] = "/" + result[item+1][1:]
                 count_mul-=1
@@ -348,7 +247,7 @@ def check_div(result):
                         y = float("-" + result[item+1])
                     else:
                         y = float(result[item+1])
-                result[item+1] = str(podeli(x,y))
+                result[item+1] = str(matematicka_knihovna.podeli(x,y))
                 if re.match(r'-', result[item+1]):
                     result[item+1] = "/" + result[item+1][1:]
                 count_div-=1
@@ -392,7 +291,7 @@ def check_plus(result):
                         y = float("-" + result[item+1])
                     else:
                         y = float(result[item+1])
-                result[item+1] = str(plus(x,y))
+                result[item+1] = str(matematicka_knihovna.plus(x,y))
                 if re.match(r'-', result[item+1]):
                     result[item+1] = "/" + result[item+1][1:]
                 count_plus-=1
@@ -436,7 +335,7 @@ def check_minus(result):
                         y = float("-" + result[item+1])
                     else:
                         y = float(result[item+1])
-                result[item+1] = str(minus(x,y))
+                result[item+1] = str(matematicka_knihovna.minus(x,y))
                 if re.match(r'-', result[item+1]):
                     result[item+1] = "/" + result[item+1][1:]
                 count_min-=1
