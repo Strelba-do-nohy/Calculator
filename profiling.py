b9336794e9628eb8cd456e9ca277a04a1dc9d0d0
20dinara03 <94635894+20dinara03@users.noreply.github.com>
2022-04-28 19:33:40 +0200

    Update profiling.py

--- profiling.py
+++ profiling.py
@@ -1,27 +1,47 @@
-#IVS_project2_kalkulacka
-#profiling
-#strelba_do_nohy
-#Garipova Dinara xgarip00
-#Datum 17.04.2022
+########################################################
+# Project name: Profiling for calculator
+# Package: Calculator
+# File: profiling.py
+# Date: 10.04.2022
+# Last changes: 28.04.2022
+# Author: Strelba do nohy (Dinara Garipova -- xgarip00)
+#########################################################
+# @package Calculator
+# @file profiling.py
+# @author Strelba do nohy (Dinara Garipova -- xgarip00)
+##########################################################
 
+# @brief To import all functions from math library and to name math library as "mk"
+# @brief To import all functions from sys
 import matematicka_knihovna as mk
 from sys import *
 
+# @param pocet-counter of inputting numbers (flag for the next functon)
 pocet = 0
+# @param cisla-array of all inputting numbers
 cisla = input().split()
+# @param classic_add - counter of sum of inputting numbers (like E(i=1,N)x(i))
 classic_add = 0.0
+# @param exponenta_add - counter of squared sum of inputting numbers
 exponenta_add = 0.0
 
+# @brief counting sum of inputting numbers (classic_add), squared sum (exponenta_add) and amount of inputting numbers
 for i in cisla:
     classic_add = mk.plus(classic_add,i)
     exponenta_add = mk.plus(exponenta_add, mk.exponenta(i,2))
-    pocet = mk.plus(pocet,i)
+    pocet = mk.plus(pocet,1)
 
+# @brief Checking if amount of all inputting numbers are 0 then write 0 to the screen
 if (pocet == 0):
     print(0)
     exit()
 
+# @param middle is the average of all inputting numbers
 middle = mk.podeli(classic_add, pocet)
+# @param odpoved is the result from the given expression
 odpoved = mk.koren(mk.minus(mk.umnoz(exponenta_add, mk.umnoz(pocet, mk.exponenta(middle,2))), mk.minus(pocet,1)))
 
 print(odpoved)
+######################################################################################################################################
+#End of file profiling.py
+#######################################################################################################################################
